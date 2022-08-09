44009673619afd3e4fe126ef5b6c55351dc8a29b
Yeren Kalibek <yerenn22@gmail.com>
2022-08-09 15:37:59 +0600

    feat (init): start celery

--- core/settings.py
+++ core/settings.py
@@ -138,6 +138,9 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
 
+CELERY_BROKER_URL = os.environ.get("CELERY_URL")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_URL")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
